80e24eeea335b5c5584b21faf6ef3fbffa3d6034
jeevan516 <67078433+jeevan516@users.noreply.github.com>
2020-06-18 00:28:19 +0200

    Add files via upload

---
+++ G1/build/CMakeFiles/welcome_message.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/welcome_message.dir/welcome_message.cc.o"
+  "libwelcome_message.pdb"
+  "libwelcome_message.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/welcome_message.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
